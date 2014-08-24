b67951c68c1cb17a959ac8e8fe7ee2db7304e285
Brendan Swiniarski <brendan.james.swiniarski@gmail.com>
2014-08-24 18:05:44 -0400

    Pushing update to README and commented analysis code.

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,31 @@
+# Gather Features labels from the correct file
 features <- read.table('features.txt', colClasses=c('integer', 'character'))
 names(features) <- c('id', 'name')
 
+# Gather Activites labels from the correct file
 activities <- read.table('activity_labels.txt', colClasses=c('integer', 'character'))
 names(activities) <- c('level', 'label')
 
 # test dataset
 test.data <- read.table('test//X_test.txt')
+# Name the columns after the Features
 names(test.data) <- features$name
 
+# Pull subject labels for each row out of the corresponding file
 test.subjects <- read.table('test//subject_test.txt')
 names(test.subjects) <- 'subject'
+# Attach subjects to dataset from test data as a factor
 test.data$subject <- factor(test.subjects$subject, 1:30)
 remove(test.subjects)
 
+# Pull the activites for each row out of the file
 test.activities <- read.table('test//y_test.txt')
 names(test.activities) <- 'activity'
+# As we did with the subjects, attach activity data to dataset as a factor
 test.data$activity <- factor(test.activities$activity, activities$level, activities$label)
 remove(test.activities)
 
-# train dataset
+# train dataset Follows the same process as above.
 train.data <- read.table('train//X_train.txt')
 names(train.data) <- features$name
 
@@ -32,23 +39,29 @@ names(train.activities) <- 'activity'
 train.data$activity <- factor(train.activities$activity, activities$level, activities$label)
 remove(train.activities)
 
+# I keep removing unnecessary tables for memory management's sake.
 remove(activities, features)
 
-# rbind
+# use rbind to combine the testing and training datasets
 data <- rbind(test.data, train.data)
 
+#now that we have one dataset, remove the other two
 remove(test.data, train.data)
 
+# Convert to data.table - this is mostly for speed
 data <- data.table(data)
 
-# remove extra cols, only keep mean() and std()
+# remove extra cols, only keep mean(), meanfreq(), and std() columns
 cols <- names(data)
 cols <- cols[cols %in% grep('std\\(\\)|mean\\(\\)|meanFreq\\(\\)', cols, value=T)]
+# make sure we have activity and subject still!
 data <- data[,c(cols, 'activity', 'subject'), with=FALSE]
 
-# mark column names with "Avg" to signify processing we've done.
-cols <- c(vapply(cols, function(x) {paste('Avg', x, sep='-')}, character(1)), 'activity', 'subject')
+# mark feature column names with "Avg" to signify processing we've done.
+cols <- c(vapply(cols, function(x) {paste('Avg', x, sep='-')}, character(1)),
+          'activity', 'subject') # keep activity and subject columns
 setnames(data, cols)
 
-#FINAL DF -> ONE ROW PER SUBJECT, PER ACTIVITY WITH ALL AVERAGE VALS
+# Final dataframe has one row per subject, per activity, with
+# 1 column each for those variables and 81 total for averaged feature data
 tidydata <- data[,lapply(.SD, mean), by=c('subject', 'activity')]
